7db7800ffcda5bf76ca93750a20b7603a68f7d6b
Aryan <aryanverma170372@gmail.com>
2022-08-18 22:52:01 +0530

    feat: Logout function implemented

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,33 @@
 import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import { getAuth } from "firebase/auth";
 
 function Profile() {
   const auth = getAuth();
+  const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
   });
 
-  return <></>;
+  const { name, email } = formData;
+
+  const onLogOut = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
+  return (
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={onLogOut}>
+          Log Out
+        </button>
+      </header>
+    </div>
+  );
 }
 
 export default Profile;
