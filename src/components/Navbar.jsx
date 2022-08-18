3be4a71230ce804c8190d6a9f0d44b57b0e5af96
Aryan <aryanverma170372@gmail.com>
2022-08-18 19:11:54 +0530

    feat: Navbar with styling on active page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,65 @@ import { ReactComponent as OfferIcon } from "../assets/svg/localOfferIcon.svg";
 import { ReactComponent as ExploreIcon } from "../assets/svg/exploreIcon.svg";
 import { ReactComponent as PersonOutlineIcon } from "../assets/svg/personOutlineIcon.svg";
 function Navbar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const pathMatchRoute = (route) => {
+    if (route == location.pathname) {
+      return true;
+    }
+  };
   return (
     <footer className="navbar">
       <nav className="navbarNav">
         <ul className="navbarListItems">
-          <l1 className="navbarListItem">
-            <ExploreIcon fill="2c2c2c" width="36px" height="36px" />
-            <p>Explore</p>
+          <l1 className="navbarListItem" onClick={() => navigate("/")}>
+            <ExploreIcon
+              fill={pathMatchRoute("/") ? "#2c2c2c" : "#8f8f8f"}
+              width="36px"
+              height="36px"
+            />
+            <p
+              className={
+                pathMatchRoute("/")
+                  ? "navbarListItemNameActive"
+                  : "navbarListItemName"
+              }
+            >
+              Explore
+            </p>
           </l1>
-          <l1 className="navbarListItem">
-            <OfferIcon fill="2c2c2c" width="36px" height="36px" />
-            <p>Offer</p>
+          <l1 className="navbarListItem" onClick={() => navigate("/offers")}>
+            <OfferIcon
+              fill={pathMatchRoute("/offers") ? "#2c2c2c" : "#8f8f8f"}
+              width="36px"
+              height="36px"
+            />
+            <p
+              className={
+                pathMatchRoute("/offers")
+                  ? "navbarListItemNameActive"
+                  : "navbarListItemName"
+              }
+            >
+              Offer
+            </p>
           </l1>
-          <l1 className="navbarListItem">
-            <PersonOutlineIcon fill="2c2c2c" width="36px" height="36px" />
-            <p>Profile</p>
+          <l1 className="navbarListItem" onClick={() => navigate("/profile")}>
+            <PersonOutlineIcon
+              fill={pathMatchRoute("/profile") ? "#2c2c2c" : "#8f8f8f"}
+              width="36px"
+              height="36px"
+            />
+            <p
+              className={
+                pathMatchRoute("/profile")
+                  ? "navbarListItemNameActive"
+                  : "navbarListItemName"
+              }
+            >
+              Profile
+            </p>
           </l1>
         </ul>
       </nav>
