c6c76d9ffa7022168e9e99faaeb93ec0f1ae7e2b
Aryan <aryanverma170372@gmail.com>
2022-08-23 00:41:25 +0530

    feat: Display & update user details

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { getAuth } from "firebase/auth";
+import { toast } from "react-toastify";
+import { getAuth, updateProfile } from "firebase/auth";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase.config";
 
 function Profile() {
   const auth = getAuth();
@@ -11,13 +14,32 @@ function Profile() {
     email: auth.currentUser.email,
   });
 
+  const [changeDetails, setChangeDetails] = useState(false);
+
   const { name, email } = formData;
 
   const onLogOut = () => {
     auth.signOut();
     navigate("/");
   };
-
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // update the displayName in firebase
+        await updateProfile(auth.currentUser, { displayName: name });
+      }
+      const userRef = doc(db, "users", auth.currentUser.uid);
+      await updateDoc(userRef, { name });
+    } catch (error) {
+      toast.error("Error updating profile");
+    }
+  };
+  const onChangeHandler = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
   return (
     <div className="profile">
       <header className="profileHeader">
@@ -26,6 +48,40 @@ function Profile() {
           Log Out
         </button>
       </header>
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && onSubmit();
+              setChangeDetails((prevState) => !prevState);
+            }}
+          >
+            {changeDetails ? "done" : "change"}
+          </p>
+        </div>
+        <div className="profileCard">
+          <form>
+            <input
+              type="text"
+              id="name"
+              className={!changeDetails ? "profileName" : "profileNameActive"}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChangeHandler}
+            />
+            <input
+              type="text"
+              id="email"
+              className={!changeDetails ? "profileEmail" : "profileEmailActive"}
+              disabled={!changeDetails}
+              value={email}
+              onChange={onChangeHandler}
+            />
+          </form>
+        </div>
+      </main>
     </div>
   );
 }
