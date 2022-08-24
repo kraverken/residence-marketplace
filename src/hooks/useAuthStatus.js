22ef2676a3b11151b7507b166e7e280d7d1ea1e4
Aryan <aryanverma170372@gmail.com>
2022-08-24 13:55:14 +0530

    warn:memory leak error

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -4,16 +4,22 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 export const useAuthStatus = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [checkingStatus, setCheckingStatus] = useState(true);
+  const isMounted = useRef(true);
 
   useEffect(() => {
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setLoggedIn(true);
-      }
-      setCheckingStatus(false);
-    });
-  });
+    if (isMounted) {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true);
+        }
+        setCheckingStatus(false);
+      });
+    }
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]);
 
   return { loggedIn, checkingStatus };
 };
