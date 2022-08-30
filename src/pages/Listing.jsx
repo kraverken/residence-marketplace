955939ab760f77605a0c6417c66c79f9f57683ba
Aryan <aryanverma170372@gmail.com>
2022-08-31 01:42:25 +0530

    fix: no need for loaction in url

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -21,7 +21,7 @@ function Listing() {
       const docSnap = await getDoc(docRef);
 
       if (docSnap.exists()) {
-        console.log(docSnap.data());
+        // console.log(docSnap.data());
         setListing(docSnap.data());
         setLoading(false);
       }
@@ -91,7 +91,7 @@ function Listing() {
 
         {auth.currentUser?.uid !== listing.userRef && (
           <Link
-            to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`}
+            to={`/contact/${listing.userRef}?listingName=${listing.name}`}
             className="primaryButton"
           >
             Contact Landlord
