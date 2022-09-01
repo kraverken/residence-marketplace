cac2b37ac86205c7e6770e447ebba927d53a4205
Aryan <aryanverma170372@gmail.com>
2022-09-01 20:09:33 +0530

    error: load more pagination(TBR)

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import {
   collection,
   getDocs,
@@ -16,8 +15,8 @@ import ListingItem from "../components/ListingItem";
 function Offers() {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [lastFetchedListing, setLastFetchedListing] = useState(null);
 
-  const params = useParams();
   useEffect(() => {
     const fetchListings = async () => {
       try {
@@ -27,12 +26,17 @@ function Offers() {
         const q = query(
           listingsRef,
           where("offer", "==", true),
-          orderBy("timestamp", "desc", limit(10))
+          orderBy("timestamp", "desc", limit(1))
         );
+        console.log(q);
         //execute query
         const querySnap = await getDocs(q);
 
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+        setLastFetchedListing(lastVisible);
+
         const listings = [];
+
         querySnap.forEach((doc) => {
           return listings.push({
             id: doc.id,
@@ -48,6 +52,43 @@ function Offers() {
     };
     fetchListings();
   }, []);
+
+  // Pagination / Load More
+  const onFetchMoreListings = async () => {
+    try {
+      // Get reference of the collection
+      const listingsRef = collection(db, "listings");
+
+      // Create a query
+      const q = query(
+        listingsRef,
+        where("offer", "==", true),
+        orderBy("timestamp", "desc"),
+        startAfter(lastFetchedListing),
+        limit(10)
+      );
+
+      // Execute query
+      const querySnap = await getDocs(q);
+
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+      setLastFetchedListing(lastVisible);
+
+      const listings = [];
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+
+      setListings((prevState) => [...prevState, ...listings]);
+      setLoading(false);
+    } catch (err) {
+      toast.error("Could not fetch listings");
+    }
+  };
 
   return (
     <div className="category">
@@ -71,6 +112,13 @@ function Offers() {
               ))}
             </ul>
           </main>
+          <br />
+          <br />
+          {lastFetchedListing && (
+            <p className="loadMore" onClick={onFetchMoreListings}>
+              Load More
+            </p>
+          )}
         </>
       ) : (
         <p>There are no current offers</p>
